1595ac54d6be2e09d82caac47fe46458e3d6b1e8
Sathwik Reddy Nalla <70497542+Sathwik-vittu@users.noreply.github.com>
2025-11-27 12:26:09 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -29,7 +29,7 @@ export const profileAPI = {
   update: (data) => api.put('/profile', data),
 };
 
-// Medication APIs
+// Medication APIS
 export const medicationAPI = {
   getAll: () => api.get('/medications'),
   create: (data) => api.post('/medications', data),
