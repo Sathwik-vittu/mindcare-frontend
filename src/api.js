cd154cb7bcce15344ee5bf01906fbd9b0117bd85
Sathwik Reddy Nalla <70497542+Sathwik-vittu@users.noreply.github.com>
2025-11-27 12:29:06 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -17,7 +17,7 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// Auth APIs
+
 export const authAPI = {
   login: (data) => api.post('/api/auth/login', data),
   register: (data) => api.post('/api/auth/register', data),
